00f6518759e7c4fd9500ff764846b42384567250
Aasif <aasif@Aasifs-MacBook-Air.local>
2026-07-14 07:52:47 +0530

    add current month income/expense, net savings, saving rate in dashboard summary api

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -6,7 +6,28 @@ export const getDashboardSummary = async (req: Request, res: Response) => {
   try {
     const userId = req.user!.id;
     if (!userId) throw new Error("User is not authorized");
-    const [totalBalance, totalIncome, totalExpense] = await Promise.all([
+    const startOfMonth = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      1,
+    );
+
+    const endOfMonth = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth() + 1,
+      0,
+      23,
+      59,
+      59,
+      999,
+    );
+    const [
+      totalBalance,
+      totalIncome,
+      totalExpense,
+      currentMonthIncome,
+      currentMonthExpense,
+    ] = await Promise.all([
       prisma.account.aggregate({
         where: { userId },
         _sum: {
@@ -25,13 +46,50 @@ export const getDashboardSummary = async (req: Request, res: Response) => {
           amount: true,
         },
       }),
+      prisma.transaction.aggregate({
+        where: {
+          userId,
+          type: TransactionType.Income,
+          transactionDate: {
+            gte: startOfMonth,
+            lte: endOfMonth,
+          },
+        },
+        _sum: {
+          amount: true,
+        },
+      }),
+      prisma.transaction.aggregate({
+        where: {
+          userId,
+          type: TransactionType.Expense,
+          transactionDate: {
+            gte: startOfMonth,
+            lte: endOfMonth,
+          },
+        },
+        _sum: {
+          amount: true,
+        },
+      }),
     ]);
+    const income = totalIncome._sum.amount ?? 0;
+    const expense = totalExpense._sum.amount ?? 0;
+
+    const netSavings = income - expense;
+    const savingsRate =
+      income > 0 ? Number(((netSavings / income) * 100).toFixed(2)) : 0;
+
     return res.status(200).json({
       success: true,
       data: {
         totalBalance: totalBalance._sum.balance ?? 0,
         totalIncome: totalIncome._sum.amount ?? 0,
         totalExpense: totalExpense._sum.amount ?? 0,
+        netSavings,
+        savingsRate,
+        currentMonthIncome: currentMonthIncome._sum.amount ?? 0,
+        currentMonthExpense: currentMonthExpense._sum.amount ?? 0,
       },
     });
   } catch (err) {
