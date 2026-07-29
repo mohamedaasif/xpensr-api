eb3724953b3bd514ef5f8f7c04acc824f4e79aa8
Aasif <aasif@Aasifs-MacBook-Air.local>
2026-07-29 19:43:49 +0530

    add dob edit support

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -32,17 +32,21 @@ export async function updateProfile(req: Request, res: Response) {
     if (lastName !== undefined) updateData.lastName = lastName;
     if (phone !== undefined) updateData.phone = phone;
     if (dob !== undefined) {
-      const regex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/\d{4}$/;
+      if (dob === null) {
+        updateData.dob = dob;
+      } else {
+        const regex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/\d{4}$/;
 
-      if (!regex.test(dob)) {
-        return res.status(400).json({
-          success: false,
-          message: "DOB must be in MM/DD/YYYY format.",
-        });
+        if (!regex.test(dob)) {
+          return res.status(400).json({
+            success: false,
+            message: "DOB must be in MM/DD/YYYY format.",
+          });
+        }
+
+        const [month, day, year] = dob.split("/").map(Number);
+        updateData.dob = new Date(Date.UTC(year, month - 1, day));
       }
-
-      const [month, day, year] = dob.split("/").map(Number);
-      updateData.dob = new Date(Date.UTC(year, month - 1, day));
     }
 
     const updatedUser = await prisma.user.update({
